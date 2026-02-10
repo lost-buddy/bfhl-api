e17a96d63acccf68f3efee91ec64291fc4d6605a
Shubham <shubham1492.be23@chitkarauniversity.edu.in>
2026-02-10 21:38:27 +0530

    Updated Gemini model

--- index.js
+++ index.js
@@ -79,26 +79,20 @@ app.post("/bfhl", async (req, res) => {
         break;
 
       case "AI":
-  if (typeof value !== "string") throw "Invalid AI input";
-
   const { GoogleGenerativeAI } = require("@google/generative-ai");
 
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
   const model = genAI.getGenerativeModel({
-    model: "gemini-pro"
+    model: "gemini-1.5-flash"
   });
 
   const result = await model.generateContent(value);
-  const response = await result.response;
-  const text = response.text();
+  const text = result.response.text();
 
-  data = text
-    .trim()
-    .split(/\s+/)[0]
-    .replace(/[.,]/g, "");
-
+  data = text.trim().split(/\s+/)[0].replace(/[.,]/g, "");
   break;
+
 
 
 
